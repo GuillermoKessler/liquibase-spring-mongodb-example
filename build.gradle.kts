f0d775ad628bfbd3e5fcf95019503d5d816c2b6c
GuillermoKessler <guillermokessler@gmail.com>
2022-07-21 21:11:34 -0300

    feat: add liquibase config with mongodb changelogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.2"
-	id("io.spring.dependency-management") version "1.0.12.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.2"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "io.kess"
@@ -12,24 +12,26 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.liquibase:liquibase-core")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.liquibase:liquibase-core:4.11.0")
+    implementation("org.liquibase.ext:liquibase-mongodb:4.13.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/io/kess/liquibasemongodb/LiquibaseMongodbApplication.kt
+++ src/main/kotlin/io/kess/liquibasemongodb/LiquibaseMongodbApplication.kt
@@ -1,11 +1,41 @@
 package io.kess.liquibasemongodb
 
+import liquibase.Contexts
+import liquibase.Liquibase
+import liquibase.database.Database
+import liquibase.database.DatabaseFactory
+import liquibase.resource.ClassLoaderResourceAccessor
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.Resource
 
 @SpringBootApplication
 class LiquibaseMongodbApplication
 
 fun main(args: Array<String>) {
-	runApplication<LiquibaseMongodbApplication>(*args)
+    runApplication<LiquibaseMongodbApplication>(*args)
+}
+
+@Configuration
+class LiquibaseConfiguration : InitializingBean {
+    @Value("\${spring.liquibase.url}")
+    private val uri: String? = null
+
+    @Value("\${spring.liquibase.change-log}")
+    private lateinit var resource: Resource
+
+    @Throws(Exception::class)
+    override fun afterPropertiesSet() {
+        val openDatabase: Database = DatabaseFactory.getInstance().openDatabase(uri, null, null, null, null, null)
+        val changelogMaster = (resource as ClassPathResource).path
+        Liquibase(
+            changelogMaster,
+            ClassLoaderResourceAccessor(),
+            openDatabase
+        ).use { liquibase -> liquibase.update(Contexts()) }
+    }
 }
